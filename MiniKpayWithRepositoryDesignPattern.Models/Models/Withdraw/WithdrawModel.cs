e36f98f7d67e93fcb6594491e3c230bf6aa06904
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 21:17:45 +0630

    Format Code

--- MiniKpayWithRepositoryDesignPattern.Models/Models/Withdraw/WithdrawModel.cs
+++ MiniKpayWithRepositoryDesignPattern.Models/Models/Withdraw/WithdrawModel.cs
@@ -1,13 +1,13 @@
-﻿namespace MiniKpayWithRepositoryDesignPattern.Models.Models.Withdraw
+﻿namespace MiniKpayWithRepositoryDesignPattern.Models.Models.Withdraw;
+
+
+public class WithdrawModel
 {
-    public class WithdrawModel
-    {
-        public long WithdrawId { get; set; }
+    public long WithdrawId { get; set; }
 
-        public string PhoneNumber { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
 
-        public decimal Balance { get; set; }
+    public decimal Balance { get; set; }
 
-        public bool DeleteFlag { get; set; }
-    }
+    public bool DeleteFlag { get; set; }
 }
