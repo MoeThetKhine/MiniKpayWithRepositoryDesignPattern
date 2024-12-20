ee34fd155f2ff1680f419a6d6c37ed1b64a091db
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 20:47:08 +0630

    -

--- MiniKpayWithRepositoryDesignPattern.Repository/Features/User/IUserRepository.cs
+++ MiniKpayWithRepositoryDesignPattern.Repository/Features/User/IUserRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.User
+﻿namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.User
 {
     internal interface IUserRepository
     {
