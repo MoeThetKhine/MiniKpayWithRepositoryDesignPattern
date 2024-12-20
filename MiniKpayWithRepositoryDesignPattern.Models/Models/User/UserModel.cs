d73989acef8fd8190d1c255fd75ccfc053c65e6c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 17:56:40 +0630

    Feat : Add UserModel

--- MiniKpayWithRepositoryDesignPattern.Models/Models/User/UserModel.cs
+++ MiniKpayWithRepositoryDesignPattern.Models/Models/User/UserModel.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace MiniKpayWithRepositoryDesignPattern.Models.Models.User
 {
-    internal class UserModel
+    public class UserModel
     {
+        public long UserId { get; set; }
+
+        public string FullName { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Pin { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public decimal Balance { get; set; }
+
+        public bool DeleteFlag { get; set; }
     }
 }
