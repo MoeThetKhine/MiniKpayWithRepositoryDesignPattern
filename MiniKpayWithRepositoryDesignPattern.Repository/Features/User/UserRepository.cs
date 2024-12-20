b8cd2513813c76e3166096d371d67217407c0487
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 01:17:41 +0630

    Feat : Add GetUserAsync in User Repository

--- MiniKpayWithRepositoryDesignPattern.Repository/Features/User/UserRepository.cs
+++ MiniKpayWithRepositoryDesignPattern.Repository/Features/User/UserRepository.cs
@@ -1,5 +1,46 @@
-﻿namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.User;
+﻿using Microsoft.EntityFrameworkCore;
+using MiniKpayWithRepositoryDesignPattern.Database.Models;
+using MiniKpayWithRepositoryDesignPattern.Models.KpayModel.User;
+using MiniKpayWithRepositoryDesignPattern.Shared;
+using MiniKpayWithRepositoryDesignPattern.Utils;
+
+namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.User;
 
 public class UserRepository : IUserRepository
 {
+    private readonly AppDbContext _db;
+
+    public UserRepository(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Result<IEnumerable<UserModel>>> GetUserAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        Result<IEnumerable<UserModel>> result;
+
+        try
+        {
+            var query = _db.TblUsers
+                .Where(x => x.DeleteFlag == false)
+                .Paginate(pageNo, pageSize);
+
+            var lst = await query.Select(x => new UserModel()
+            {
+                UserId = x.UserId,
+                FullName = x.FullName,
+                Pin = x.Pin,
+                PhoneNumber = x.PhoneNumber,
+                Balance = x.Balance
+            }).ToListAsync();
+
+            result = Result<IEnumerable<UserModel>>.Success();
+        }
+        catch (Exception ex)
+        {
+            result = Result<IEnumerable<UserModel>>.Fail(ex.Message);
+        }
+        return result;
+
+    }
 }
