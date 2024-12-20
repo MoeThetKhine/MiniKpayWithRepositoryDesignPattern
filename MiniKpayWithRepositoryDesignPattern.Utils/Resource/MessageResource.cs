862f58e15e07b37c3f8cc817468afcd6e5440b96
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 17:59:49 +0630

    -

--- MiniKpayWithRepositoryDesignPattern.Utils/Resource/MessageResource.cs
+++ MiniKpayWithRepositoryDesignPattern.Utils/Resource/MessageResource.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniKpayWithRepositoryDesignPattern.Utils.Resource
+﻿namespace MiniKpayWithRepositoryDesignPattern.Utils.Resource
 {
     public class MessageResource
     {
