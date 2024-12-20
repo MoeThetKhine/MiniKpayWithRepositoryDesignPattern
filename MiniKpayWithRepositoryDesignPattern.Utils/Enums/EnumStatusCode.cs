5c836a56105d8445a04dc2a08a069e651f67b04f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 18:04:05 +0630

    Format Code

--- MiniKpayWithRepositoryDesignPattern.Utils/Enums/EnumStatusCode.cs
+++ MiniKpayWithRepositoryDesignPattern.Utils/Enums/EnumStatusCode.cs
@@ -1,13 +1,11 @@
-﻿namespace MiniKpayWithRepositoryDesignPattern.Utils.Enums
-{
-    public enum EnumStatusCode
-    {
-        None,
-        Success = 200,
-        BadRequest = 400,
-        Conflict = 409,
-        NotFound = 404,
-        InternalServerError = 500
-    }
+﻿namespace MiniKpayWithRepositoryDesignPattern.Utils.Enums;
 
+public enum EnumStatusCode
+{
+    None,
+    Success = 200,
+    BadRequest = 400,
+    Conflict = 409,
+    NotFound = 404,
+    InternalServerError = 500
 }
