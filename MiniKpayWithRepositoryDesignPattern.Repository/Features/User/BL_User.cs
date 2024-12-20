387dc9e92a7f52b27bf59a539898a4b400f20d88
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 01:18:01 +0630

    Feat : Add GetUserAsync in BL_User

--- MiniKpayWithRepositoryDesignPattern.Repository/Features/User/BL_User.cs
+++ MiniKpayWithRepositoryDesignPattern.Repository/Features/User/BL_User.cs
@@ -1,5 +1,28 @@
-﻿namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.User;
+﻿using MiniKpayWithRepositoryDesignPattern.Models.KpayModel.User;
+using MiniKpayWithRepositoryDesignPattern.Utils;
+
+namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.User;
 
 public class BL_User
 {
+    private readonly UserRepository _userRepository;
+
+    public BL_User(UserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    public async Task<Result<IEnumerable<UserModel>>> GetUserAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        Result<IEnumerable<UserModel>> response;
+        try
+        {
+            response = await _userRepository.GetUserAsync(pageNo, pageSize, cs);
+        }
+        catch (Exception ex)
+        {
+            response =  Result<IEnumerable<UserModel>>.Fail(ex);
+        }
+        result: 
+        return response;
+    }
 }
