aa43f81817504a0504b994045639c0767211a818
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 17:56:54 +0630

    Feat : Add Withdraw Model

--- MiniKpayWithRepositoryDesignPattern.Models/Models/Withdraw/WithdrawModel.cs
+++ MiniKpayWithRepositoryDesignPattern.Models/Models/Withdraw/WithdrawModel.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace MiniKpayWithRepositoryDesignPattern.Models.Models.Withdraw
 {
-    internal class WithdrawModel
+    public class WithdrawModel
     {
+        public long WithdrawId { get; set; }
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public decimal Balance { get; set; }
+
+        public bool DeleteFlag { get; set; }
     }
 }
