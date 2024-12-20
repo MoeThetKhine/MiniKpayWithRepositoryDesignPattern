6033a6420dfb91d2e798fa8c3d5dd8c5c34b6f35
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 01:18:12 +0630

    Feat : Add GetUserAsync in Controller

--- MiniKpayWithRepositoryDesignPattern.RestApi/Controllers/User/UserController.cs
+++ MiniKpayWithRepositoryDesignPattern.RestApi/Controllers/User/UserController.cs
@@ -7,5 +7,18 @@ namespace MiniKpayWithRepositoryDesignPattern.RestApi.Controllers.User
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly BL_User _user;
+
+        public UserController(BL_User user)
+        {
+            _user = user;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserAsync(int pageNo, int pageSize, CancellationToken cs)
+        {
+            var result = _user.GetUserAsync(pageNo, pageSize, cs);
+            return Ok(result);
+        }
     }
 }
