bfb7a59980eddcfeb3bcc4a8a48bc1f997b14287
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 18:00:42 +0630

    -

--- MiniKpayWithRepositoryDesignPattern.Utils/Result.cs
+++ MiniKpayWithRepositoryDesignPattern.Utils/Result.cs
@@ -1,9 +1,4 @@
 ﻿using MiniKpayWithRepositoryDesignPattern.Utils.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniKpayWithRepositoryDesignPattern.Utils
 {
