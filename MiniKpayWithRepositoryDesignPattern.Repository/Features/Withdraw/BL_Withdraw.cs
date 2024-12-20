892fceeab806dfc3d70b6c9a1b685588f2251234
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 20:46:04 +0630

    -

--- MiniKpayWithRepositoryDesignPattern.Repository/Features/Withdraw/BL_Withdraw.cs
+++ MiniKpayWithRepositoryDesignPattern.Repository/Features/Withdraw/BL_Withdraw.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.Withdraw
+﻿namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.Withdraw
 {
     internal class BL_Withdraw
     {
