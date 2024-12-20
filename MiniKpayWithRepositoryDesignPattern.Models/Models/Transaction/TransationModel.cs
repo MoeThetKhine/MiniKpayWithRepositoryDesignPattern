42a4af0a351b9cf64e72cf64211c329d5b1384d0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 17:56:19 +0630

    Feat : Add Transaction Model

--- MiniKpayWithRepositoryDesignPattern.Models/Models/Transaction/TransationModel.cs
+++ MiniKpayWithRepositoryDesignPattern.Models/Models/Transaction/TransationModel.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace MiniKpayWithRepositoryDesignPattern.Models.Models.Transaction
 {
-    internal class TransationModel
+    public class TransationModel
     {
+        public long TransactionId { get; set; }
+
+        public string FromPhoneNumber { get; set; } = null!;
+
+        public string ToPhoneNumber { get; set; } = null!;
+
+        public decimal Amount { get; set; }
+
+        public string Pin { get; set; } = null!;
+
+        public bool DeleteFlag { get; set; }
     }
 }
