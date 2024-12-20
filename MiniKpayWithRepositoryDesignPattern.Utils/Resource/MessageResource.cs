f55ec753f57fff9eda7d4b6190556eb07d8b8d3b
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 18:00:27 +0630

    -

--- MiniKpayWithRepositoryDesignPattern.Utils/Resource/MessageResource.cs
+++ MiniKpayWithRepositoryDesignPattern.Utils/Resource/MessageResource.cs
@@ -1,7 +1,11 @@
 ﻿namespace MiniKpayWithRepositoryDesignPattern.Utils.Resource;
 
+#region MessageResource
+
 public class MessageResource
 {
     public static string Success { get; } = "Success";
     public static string Fail { get; } = "Fail.";
 }
+
+#endregion
