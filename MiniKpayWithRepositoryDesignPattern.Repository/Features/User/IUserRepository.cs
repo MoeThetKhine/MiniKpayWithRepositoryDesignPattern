31e5ccd8cdadc2af7bb02056e218f8cdca69b182
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 23:41:05 +0630

    -

--- MiniKpayWithRepositoryDesignPattern.Repository/Features/User/IUserRepository.cs
+++ MiniKpayWithRepositoryDesignPattern.Repository/Features/User/IUserRepository.cs
@@ -1,5 +1,9 @@
-﻿namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.User;
+﻿using MiniKpayWithRepositoryDesignPattern.Models.KpayModel.User;
+using MiniKpayWithRepositoryDesignPattern.Utils;
+
+namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.User;
 
 public interface IUserRepository
 {
+    Task<Result<IEnumerable<UserModel>>> GetUserAsync(int pageNo, int pageSize, CancellationToken cs);
 }
