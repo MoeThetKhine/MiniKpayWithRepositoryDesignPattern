9ce5d9344631f3597c4579e531c5fe07a2a443ed
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 21:17:24 +0630

    -

--- MiniKpayWithRepositoryDesignPattern.Models/Models/Withdraw/WithdrawModel.cs
+++ MiniKpayWithRepositoryDesignPattern.Models/Models/Withdraw/WithdrawModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniKpayWithRepositoryDesignPattern.Models.Models.Withdraw
+﻿namespace MiniKpayWithRepositoryDesignPattern.Models.Models.Withdraw
 {
     public class WithdrawModel
     {
