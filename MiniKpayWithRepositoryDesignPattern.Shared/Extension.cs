00f0ba8df4520eb7e89f568371cec7b45daec216
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 21:02:59 +0630

    Feat : Add Extensions

--- MiniKpayWithRepositoryDesignPattern.Shared/Extension.cs
+++ MiniKpayWithRepositoryDesignPattern.Shared/Extension.cs
@@ -1,6 +1,10 @@
 ﻿namespace MiniKpayWithRepositoryDesignPattern.Shared
 {
-    public class Extension
+    public static class Extensions
     {
+        public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> sources, int pageNo, int pageSize)
+        {
+            return sources.Skip((pageNo - 1) * pageSize).Take(pageSize);
+        }
     }
 }
