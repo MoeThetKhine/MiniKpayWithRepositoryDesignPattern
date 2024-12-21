2aea46628a5956a8bfc4b5e947cb1e83e8b0b590
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-22 00:01:39 +0630

    Feat : Add CreateTransactionAsync in TransactionRepository

--- MiniKpayWithRepositoryDesignPattern.Repository/Features/Transaction/TransationRepository.cs
+++ MiniKpayWithRepositoryDesignPattern.Repository/Features/Transaction/TransationRepository.cs
@@ -1,5 +1,37 @@
-﻿namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.Transaction;
+﻿using MiniKpayWithRepositoryDesignPattern.Models.KpayModel.Transaction;
 
-public class TransationRepository: ITransactionRepository
+namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.Transaction;
+
+public class TransationRepository : ITransactionRepository
 {
+    private readonly AppDbContext _appDbContext;
+
+    public async Task<Result<TransactionRequestModel>> CreateTransactionAsync(TransactionRequestModel request, CancellationToken cs)
+    {
+        Result<TransactionRequestModel> result;
+
+        try
+        {
+            
+            var transaction = new TblTransaction
+            {
+                FromPhoneNumber = request.FromPhoneNumber,
+                ToPhoneNumber = request.ToPhoneNumber,
+                Amount = request.Amount.Value,
+                Pin = request.Pin,
+            };
+
+            _appDbContext.Add(transaction);
+            await _appDbContext.SaveChangesAsync();
+
+            result = Result<TransactionRequestModel>.Success(request);
+            
+
+        }
+        catch (Exception ex)
+        {
+            result = Result<TransactionRequestModel>.Fail(ex.Message);
+        }
+        return result;
+    }
 }
