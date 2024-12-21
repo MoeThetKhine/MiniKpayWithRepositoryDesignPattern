7cef60a1e6e94028941490f153cc2764387071af
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-22 00:02:19 +0630

    Feat : Add CreateTransactionAsync in TransactionController

--- MiniKpayWithRepositoryDesignPattern.RestApi/Controllers/Transaction/TransactionController.cs
+++ MiniKpayWithRepositoryDesignPattern.RestApi/Controllers/Transaction/TransactionController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MiniKpayWithRepositoryDesignPattern.Models.KpayModel.Transaction;
 
 namespace MiniKpayWithRepositoryDesignPattern.RestApi.Controllers.Transaction
 {
@@ -7,5 +8,17 @@ namespace MiniKpayWithRepositoryDesignPattern.RestApi.Controllers.Transaction
     [ApiController]
     public class TransactionController : ControllerBase
     {
+        private readonly BL_Transaction _bL_Transaction;
+
+        public TransactionController(BL_Transaction bL_Transaction)
+        {
+            _bL_Transaction = bL_Transaction;
+        }
+        public async Task<IActionResult>CreateTransactionAsync(TransactionRequestModel request, CancellationToken cs)
+        {
+            var transaction = await _bL_Transaction.CreateTransactionAsync(request, cs);
+            return Ok(transaction);
+
+        }
     }
 }
