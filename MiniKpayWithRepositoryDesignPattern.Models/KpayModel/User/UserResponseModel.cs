33908039345f705df48f6d1c88a66cadb7b2be07
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 13:40:41 +0630

    Feat : Add UserResponseModel

---
+++ MiniKpayWithRepositoryDesignPattern.Models/KpayModel/User/UserResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniKpayWithRepositoryDesignPattern.Models.KpayModel.User
+{
+    public class UserResponseModel
+    {
+        public string FullName { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Pin { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
