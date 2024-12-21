2faa5b0d2ddb8f8ae9b946e9f1217f987bbcef6a
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-22 00:35:53 +0630

    -

--- MiniKpayWithRepositoryDesignPattern.Repository/Features/Transaction/ITransactionRepository.cs
+++ MiniKpayWithRepositoryDesignPattern.Repository/Features/Transaction/ITransactionRepository.cs
@@ -1,7 +1,11 @@
 ﻿namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.Transaction;
 
+#region ITransactionRepository
+
 public interface ITransactionRepository
 {
     Task<Result<TransactionRequestModel>> CreateTransactionAsync(TransactionRequestModel transactionRequest,CancellationToken cs);
     Task<Result<List<TransationModel>>> GetTransactionAsync(CancellationToken cs);
 }
+
+#endregion
