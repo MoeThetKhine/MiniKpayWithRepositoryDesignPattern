9a246a9341f3f3d216586b4818d034fb5bde6c8f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-22 00:02:02 +0630

    Feat : Add CreateTransactionAsync in BL_Transaction

--- MiniKpayWithRepositoryDesignPattern.Repository/Features/Transaction/BL_Transaction.cs
+++ MiniKpayWithRepositoryDesignPattern.Repository/Features/Transaction/BL_Transaction.cs
@@ -1,5 +1,70 @@
-﻿namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.Transaction;
+﻿using MiniKpayWithRepositoryDesignPattern.Database.Models;
+using MiniKpayWithRepositoryDesignPattern.Models.KpayModel.Transaction;
+using MiniKpayWithRepositoryDesignPattern.Utils;
+
+namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.Transaction;
 
 public class BL_Transaction
 {
+    private readonly ITransactionRepository _transactionRepository;
+    private readonly AppDbContext _appDbContext;
+
+
+    public BL_Transaction(ITransactionRepository transactionRepository)
+    {
+        _transactionRepository = transactionRepository;
+    }
+
+
+   
+    public async Task<Result<TransactionRequestModel>> CreateTransactionAsync(TransactionRequestModel request, CancellationToken cs)
+    {
+        Result<TransactionRequestModel> response;
+
+        try
+        {
+            if (request.Amount is null || request.Amount <= 0)
+            {
+                response = Result<TransactionRequestModel>.Fail("Transaction amount must be greater than 0.");
+            }
+
+            var sender = await _appDbContext.TblUsers.FirstOrDefaultAsync(x => x.PhoneNumber == request.FromPhoneNumber);
+            var receiver = await _appDbContext.TblUsers.FirstOrDefaultAsync(x => x.PhoneNumber == request.ToPhoneNumber);
+
+            if (request.FromPhoneNumber == request.ToPhoneNumber)
+            {
+                response = Result<TransactionRequestModel>.Fail("Sender and Receiver Phone Number must be different.");
+            }
+
+            if (sender is null)
+            {
+                response = Result<TransactionRequestModel>.Fail("Sender Phone Number does not exist.");
+            }
+            if (receiver is null)
+            {
+                response = Result<TransactionRequestModel>.Fail("Receiver Phone Number does not exists");
+            }
+            if (sender.Balance < request.Amount)
+            {
+                response = Result<TransactionRequestModel>.Fail("Insufficient balance.");
+            }
+            sender.Balance -= request.Amount.Value;
+            receiver.Balance += request.Amount.Value;
+
+            _appDbContext.TblUsers.Update(sender);
+            _appDbContext.TblUsers.Update(receiver);
+
+            await _appDbContext.SaveChangesAsync();
+
+
+            response = await _transactionRepository.CreateTransactionAsync(request, cs);
+
+        }
+        catch (Exception ex)
+        {
+            response = Result<TransactionRequestModel>.Fail(ex);
+        }
+    result:
+        return response;
+    }
 }
