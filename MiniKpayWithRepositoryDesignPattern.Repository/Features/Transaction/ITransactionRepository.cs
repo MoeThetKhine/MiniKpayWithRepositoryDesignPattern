a710c2fb54889c111bfec81e0e9b718894c44e1e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-22 00:26:37 +0630

    Feat : Add GetTransactionAsync in ITransactionRepository

--- MiniKpayWithRepositoryDesignPattern.Repository/Features/Transaction/ITransactionRepository.cs
+++ MiniKpayWithRepositoryDesignPattern.Repository/Features/Transaction/ITransactionRepository.cs
@@ -5,4 +5,5 @@ namespace MiniKpayWithRepositoryDesignPattern.Repository.Features.Transaction;
 public interface ITransactionRepository
 {
     Task<Result<TransactionRequestModel>> CreateTransactionAsync(TransactionRequestModel transactionRequest,CancellationToken cs);
+    Task<Result<List<TransationModel>>> GetTransactionAsync(CancellationToken cs);
 }
