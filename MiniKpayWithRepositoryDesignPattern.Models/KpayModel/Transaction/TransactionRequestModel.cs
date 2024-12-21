9cf5aa492c224ef762a7d10e95d10a72f286674d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 17:10:06 +0630

    Format Code

--- MiniKpayWithRepositoryDesignPattern.Models/KpayModel/Transaction/TransactionRequestModel.cs
+++ MiniKpayWithRepositoryDesignPattern.Models/KpayModel/Transaction/TransactionRequestModel.cs
@@ -1,14 +1,13 @@
-﻿namespace MiniKpayWithRepositoryDesignPattern.Models.KpayModel.Transaction
+﻿namespace MiniKpayWithRepositoryDesignPattern.Models.KpayModel.Transaction;
+
+public class TransactionRequestModel
 {
-    public class TransactionRequestModel
-    {
 
-        public string FromPhoneNumber { get; set; } = null!;
+    public string FromPhoneNumber { get; set; } = null!;
 
-        public string ToPhoneNumber { get; set; } = null!;
+    public string ToPhoneNumber { get; set; } = null!;
 
-        public decimal Amount { get; set; }
+    public decimal Amount { get; set; }
 
-        public string Pin { get; set; } = null!;
-    }
+    public string Pin { get; set; } = null!;
 }
