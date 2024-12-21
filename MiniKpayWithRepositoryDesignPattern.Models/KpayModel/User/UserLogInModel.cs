dbf6511ba3e9d1658f16983d7217819a0ea732bc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 16:35:43 +0630

    -

--- MiniKpayWithRepositoryDesignPattern.Models/KpayModel/User/UserLogInModel.cs
+++ MiniKpayWithRepositoryDesignPattern.Models/KpayModel/User/UserLogInModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace MiniKpayWithRepositoryDesignPattern.Models.KpayModel.User;
 
+#region UserLogInModel
+
 public class UserLogInModel
 {
     public string FullName { get; set; } = null!;
 
     public string Password { get; set; } = null!;
 }
+
+#endregion
