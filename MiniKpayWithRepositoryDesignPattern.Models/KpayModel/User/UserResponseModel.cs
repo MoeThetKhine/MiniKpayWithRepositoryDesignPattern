391ffd57d58e59ad96129feb0ae450dc563f0995
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 16:33:37 +0630

    Format Code

--- MiniKpayWithRepositoryDesignPattern.Models/KpayModel/User/UserResponseModel.cs
+++ MiniKpayWithRepositoryDesignPattern.Models/KpayModel/User/UserResponseModel.cs
@@ -1,13 +1,12 @@
-﻿namespace MiniKpayWithRepositoryDesignPattern.Models.KpayModel.User
+﻿namespace MiniKpayWithRepositoryDesignPattern.Models.KpayModel.User;
+
+public class UserResponseModel
 {
-    public class UserResponseModel
-    {
-        public string FullName { get; set; } = null!;
+    public string FullName { get; set; } = null!;
 
-        public string Password { get; set; } = null!;
+    public string Password { get; set; } = null!;
 
-        public string Pin { get; set; } = null!;
+    public string Pin { get; set; } = null!;
 
-        public string PhoneNumber { get; set; } = null!;
-    }
+    public string PhoneNumber { get; set; } = null!;
 }
