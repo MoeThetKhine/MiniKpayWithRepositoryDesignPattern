80d99fcfb371ba5c34c2070e7b6255cd7d5227d8
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 16:30:58 +0630

    Feat : Add UserLoginModel

---
+++ MiniKpayWithRepositoryDesignPattern.Models/KpayModel/User/UserLogInModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniKpayWithRepositoryDesignPattern.Models.KpayModel.User
+{
+    public class UserLogInModel
+    {
+        public string FullName { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
