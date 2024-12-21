7bf0d8e2f3cc580d07316578a2debdfdf84c6a81
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-22 00:30:19 +0630

    Format Code

--- MiniKpayWithRepositoryDesignPattern.RestApi/Controllers/Transaction/TransactionController.cs
+++ MiniKpayWithRepositoryDesignPattern.RestApi/Controllers/Transaction/TransactionController.cs
@@ -1,30 +1,29 @@
 ﻿using MiniKpayWithRepositoryDesignPattern.Models.KpayModel.Transaction;
 
-namespace MiniKpayWithRepositoryDesignPattern.RestApi.Controllers.Transaction
+namespace MiniKpayWithRepositoryDesignPattern.RestApi.Controllers.Transaction;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TransactionController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TransactionController : ControllerBase
+    private readonly BL_Transaction _bL_Transaction;
+
+    public TransactionController(BL_Transaction bL_Transaction)
     {
-        private readonly BL_Transaction _bL_Transaction;
+        _bL_Transaction = bL_Transaction;
+    }
 
-        public TransactionController(BL_Transaction bL_Transaction)
-        {
-            _bL_Transaction = bL_Transaction;
-        }
+    [HttpPost]
+    public async Task<IActionResult>CreateTransactionAsync(TransactionRequestModel request, CancellationToken cs)
+    {
+        var transaction = await _bL_Transaction.CreateTransactionAsync(request, cs);
+        return Ok(transaction);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult>CreateTransactionAsync(TransactionRequestModel request, CancellationToken cs)
-        {
-            var transaction = await _bL_Transaction.CreateTransactionAsync(request, cs);
-            return Ok(transaction);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult>GetTransactionAsync(CancellationToken cs)
-        {
-            var transaction = await _bL_Transaction.GetTransactionAsync(cs);
-            return Ok(transaction);
-        }
+    [HttpGet]
+    public async Task<IActionResult>GetTransactionAsync(CancellationToken cs)
+    {
+        var transaction = await _bL_Transaction.GetTransactionAsync(cs);
+        return Ok(transaction);
     }
 }
