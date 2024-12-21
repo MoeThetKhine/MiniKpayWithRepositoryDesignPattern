776a7b296aee2f35b510d74ed88bb2b3b593b29d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-22 00:30:05 +0630

    -

--- MiniKpayWithRepositoryDesignPattern.RestApi/Controllers/Transaction/TransactionController.cs
+++ MiniKpayWithRepositoryDesignPattern.RestApi/Controllers/Transaction/TransactionController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using MiniKpayWithRepositoryDesignPattern.Models.KpayModel.Transaction;
+﻿using MiniKpayWithRepositoryDesignPattern.Models.KpayModel.Transaction;
 
 namespace MiniKpayWithRepositoryDesignPattern.RestApi.Controllers.Transaction
 {
