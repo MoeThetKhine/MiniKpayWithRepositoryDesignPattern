6895e68dcfbb135f5177ac6baa4535aaf8216e9d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 11:45:56 +0630

    Feat : Add User Request Model

---
+++ MiniKpayWithRepositoryDesignPattern.Models/KpayModel/User/UserRequestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniKpayWithRepositoryDesignPattern.Models.KpayModel.User
+{
+    public class UserRequestModel
+    {
+        public string FullName { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Pin { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public decimal Balance { get; set; }
+
+    }
+}
