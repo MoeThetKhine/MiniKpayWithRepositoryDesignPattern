b85948471ad44b4215b737e5673ea54a98650b8f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 17:09:05 +0630

    Feat : Add TransactionRequestModel

---
+++ MiniKpayWithRepositoryDesignPattern.Models/KpayModel/Transaction/TransactionRequestModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniKpayWithRepositoryDesignPattern.Models.KpayModel.Transaction
+{
+    public class TransactionRequestModel
+    {
+
+        public string FromPhoneNumber { get; set; } = null!;
+
+        public string ToPhoneNumber { get; set; } = null!;
+
+        public decimal Amount { get; set; }
+
+        public string Pin { get; set; } = null!;
+    }
+}
