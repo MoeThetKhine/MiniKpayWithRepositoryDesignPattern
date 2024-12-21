073d7ce8a55a93c1a6a652f3003d35716ab2925d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 16:46:01 +0630

    Format Code

--- MiniKpayWithRepositoryDesignPattern.RestApi/Controllers/User/UserController.cs
+++ MiniKpayWithRepositoryDesignPattern.RestApi/Controllers/User/UserController.cs
@@ -1,47 +1,46 @@
-﻿namespace MiniKpayWithRepositoryDesignPattern.RestApi.Controllers.User
+﻿namespace MiniKpayWithRepositoryDesignPattern.RestApi.Controllers.User;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
+    private readonly BL_User _user;
+
+    public UserController(BL_User user)
     {
-        private readonly BL_User _user;
+        _user = user;
+    }
 
-        public UserController(BL_User user)
-        {
-            _user = user;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetUserAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        var result = await _user.GetUserAsync(pageNo, pageSize, cs);
+        return Ok(result);
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetUserAsync(int pageNo, int pageSize, CancellationToken cs)
-        {
-            var result = await _user.GetUserAsync(pageNo, pageSize, cs);
-            return Ok(result);
-        }
+    [HttpPost]
+    public async Task<IActionResult>CreateUserAsync([FromForm]UserRequestModel userRequestModel, CancellationToken cs)
+    {
+        var result = await _user.CreateUserAsync(userRequestModel, cs);
+        return Ok(result);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult>CreateUserAsync([FromForm]UserRequestModel userRequestModel, CancellationToken cs)
-        {
-            var result = await _user.CreateUserAsync(userRequestModel, cs);
-            return Ok(result);
-        }
-
-        [HttpPatch]
-        public async Task<IActionResult>UpdateUserProfileAsync(string phono, UserResponseModel responseModel,CancellationToken cs)
-        {
-            var result = await _user.UpdateUserProfileAsync(phono, responseModel, cs);
-            return Ok(result);
-        }
-        [HttpDelete]
-        public async Task<IActionResult> LogOutUserAsync(string phno, CancellationToken cs)
-        {
-            var result = await _user.LogOutUserAsync(phno, cs);
-            return Ok(result);
-        }
-        [HttpPut]
-        public async Task<IActionResult> LogInUserAsync(UserLogInModel userLogInModel, CancellationToken cs)
-        {
-            var result = await _user.LogInUserAsync(userLogInModel, cs);
-            return Ok(result);
-        }
+    [HttpPatch]
+    public async Task<IActionResult>UpdateUserProfileAsync(string phono, UserResponseModel responseModel,CancellationToken cs)
+    {
+        var result = await _user.UpdateUserProfileAsync(phono, responseModel, cs);
+        return Ok(result);
+    }
+    [HttpDelete]
+    public async Task<IActionResult> LogOutUserAsync(string phno, CancellationToken cs)
+    {
+        var result = await _user.LogOutUserAsync(phno, cs);
+        return Ok(result);
+    }
+    [HttpPut]
+    public async Task<IActionResult> LogInUserAsync(UserLogInModel userLogInModel, CancellationToken cs)
+    {
+        var result = await _user.LogInUserAsync(userLogInModel, cs);
+        return Ok(result);
     }
 }
