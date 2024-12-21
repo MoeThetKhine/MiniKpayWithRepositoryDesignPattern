5322e1650df2656a1dfd546a3e5fb4e8980abc1f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-21 14:00:30 +0630

    Feat : Add LogOutUserAsync in IUserRepository

--- MiniKpayWithRepositoryDesignPattern.Repository/Features/User/IUserRepository.cs
+++ MiniKpayWithRepositoryDesignPattern.Repository/Features/User/IUserRepository.cs
@@ -8,4 +8,5 @@ public interface IUserRepository
     Task<Result<List<UserModel>>> GetUserAsync(int pageNo, int pageSize, CancellationToken cs);
     Task<Result<UserRequestModel>> CreateUserAsync(UserRequestModel userRequestModel, CancellationToken cs);
     Task<Result<UserResponseModel>> UpdateUserProfileAsync(string phno, UserResponseModel responseModel, CancellationToken cs);
+    Task<Result<UserModel>> LogOutUserAsync(string phno, CancellationToken cs);
 }
